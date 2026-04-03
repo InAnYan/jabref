be3eb71771027252227bdad7dae1c7e7c978e5a7
InAnYan <ruslanpopov1512@gmail.com>
2026-04-03 11:50:39 +0200

    refactor(ai): start AiSummarizationLogic

---
+++ jablib/src/main/java/org/jabref/logic/ai/summarization/logic/AiSummarizationLogic.java
@@ -0,0 +1,11 @@
+package org.jabref.logic.ai.summarization.logic;
+
+import javafx.beans.property.ObjectProperty;
+
+import org.jabref.logic.ai.summarization.logic.summarizationalgorithms.Summarizator;
+import org.jabref.model.ai.summarization.SummarizatorKind;
+
+public class AiSummarizationLogic {
+    private final ObjectProperty<SummarizatorKind> summarizatorKind;
+    private final ObjectProperty<Summarizator> summarizator;
+}
