e6533e5690586a997e69ec3896b84ed10a236420
InAnYan <ruslanpopov1512@gmail.com>
2026-04-17 14:07:57 +0200

    refactor(ai): refactor EmbeddingSimilarityMetric

--- jablib/src/main/java/org/jabref/model/ai/embeddings/EmbeddingSimilarityMetric.java
+++ jablib/src/main/java/org/jabref/model/ai/embeddings/EmbeddingSimilarityMetric.java
@@ -3,12 +3,15 @@ package org.jabref.model.ai.embeddings;
 import org.jabref.logic.l10n.Localization;
 
 public enum EmbeddingSimilarityMetric {
-    COSINE_SIMILARITY;
+    COSINE_SIMILARITY(Localization.lang("Cosine Similarity"));
+
+    private final String displayName;
+
+    EmbeddingSimilarityMetric(String displayName) {
+        this.displayName = displayName;
+    }
 
     public String getDisplayName() {
-        return switch (this) {
-            case COSINE_SIMILARITY ->
-                    Localization.lang("Cosine Similarity");
-        };
+        return displayName;
     }
 }
